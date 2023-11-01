dfd59b97008f3b10f7a32ba334970c26eb474ac2
thomazblack <thomazgabriel42@hotmail.com>
2023-11-01 18:51:59 -0300

    concertado

--- assets/js/apagar.js
+++ assets/js/apagar.js
@@ -5,18 +5,13 @@
     //const tarefas_filtradas =  tarefas.filter(tarefa => tarefa.id !== id)
 
     //salvar o localstorage
-<<<<<<< HEAD
+
     //localStorage.setItem("tarefas",JSON.stringify (tarefas_filtradas))
 
-
-function apagar(id){
-    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
-    const tarefas_filtradas = tarefas.filter(tarefa => tarefa.id !== id)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas_filtradas) )
-    atualizar()
-    calcularEstatisticas()
-=======
-    localStorage.setItem("tarefas",JSON.stringify (tarefas_filtradas))
-    calcularEstatiscas()
->>>>>>> 825ac178b93f91714b48886d9ac2b7871d9deefe
-}
+    function apagar(id){
+        const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+        const tarefas_filtradas = tarefas.filter(tarefa => tarefa.id !== id)
+        localStorage.setItem("tarefas", JSON.stringify(tarefas_filtradas) )
+        atualizar()
+        calcularEstatisticas()
+    }
--- assets/js/carregar.js
+++ assets/js/carregar.js
@@ -2,57 +2,56 @@
     
  //   function atualizar(){
 
-<<<<<<< HEAD
+
    //   document.querySelector("#lista-tarefas").innerHTML = ""
     //  let tarefas =JSON.parse(localStorage.getItem("tarefas")) || []
   //    console.log(tarefas)
     //  tarefas.forEach (tarefa=> criaCard (tarefa))
    // }
-   window.addEventListener("load", atualizar )
-=======
-      document.querySelector("#lista-tarefas").innerHTML = ""
-      let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
-      tarefas.forEach(tarefa => criarCard(tarefa))
-  }
+  // window.addEventListener("load", atualizar )
+
+     // document.querySelector("#lista-tarefas").innerHTML = ""
+      //let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+     // tarefas.forEach(tarefa => criarCard(tarefa))
 
     //  document.querySelector("#lista-tarefas").innerHTML = ""
     //  let tarefas =JSON.parse(localStorage.getItem("tarefas")) || []
     //  console.log(tarefas)
     //  tarefas.forEach (tarefa=> criaCard (tarefa))
    
->>>>>>> 825ac178b93f91714b48886d9ac2b7871d9deefe
 
-   function atualizar(){
-       document.querySelector("#lista-tarefas").innerHTML = ""
-       let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
-       tarefas.forEach(tarefa => criarCard(tarefa))
-   }
-   
-   function criarCard(tarefa){
-       const card = document.createElement("div")
-       card.classList.add("col", "s12", "m6", "l4")
-   
-       card.innerHTML = `
-           <div class="card ${tarefa.concluida ? 'grey': ''}">
-               <div class="card-content">
-               <span class="card-title">${tarefa.titulo}</span>
-               <p>${tarefa.descricao}</p>
-               <span data-badge-caption="pontos" class="badge blue white-text">
-                   ${tarefa.pontos}
-               </span>
-               </div>
-               <div class="card-action">
-               <a href="#" class="btn red" onClick="apagar(${tarefa.id})">
-                   <i class="material-icons">delete</i>
-                   </a>
-                   <a href="#" class="btn green" onClick="concluir(${tarefa.id})">
-                       <i class="material-icons">check</i>
-                   </a>
-               </div>
-           </div>
-       `
-   
-       document.querySelector("#lista-tarefas").appendChild(card)
-   
-   }
+    window.addEventListener("load", atualizar )
 
+function atualizar(){
+    document.querySelector("#lista-tarefas").innerHTML = ""
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    tarefas.forEach(tarefa => criarCard(tarefa))
+}
+
+function criarCard(tarefa){
+    const card = document.createElement("div")
+    card.classList.add("col", "s12", "m6", "l4")
+
+    card.innerHTML = `
+        <div class="card ${tarefa.concluida ? 'grey': ''}">
+            <div class="card-content">
+            <span class="card-title">${tarefa.titulo}</span>
+            <p>${tarefa.descricao}</p>
+            <span data-badge-caption="pontos" class="badge blue white-text">
+                ${tarefa.pontos}
+            </span>
+            </div>
+            <div class="card-action">
+            <a href="#" class="btn red" onClick="apagar(${tarefa.id})">
+                <i class="material-icons">delete</i>
+                </a>
+                <a href="#" class="btn green" onClick="concluir(${tarefa.id})">
+                    <i class="material-icons">check</i>
+                </a>
+            </div>
+        </div>
+    `
+
+    document.querySelector("#lista-tarefas").appendChild(card)
+
+}
